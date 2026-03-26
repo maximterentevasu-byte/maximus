5bf4d88150b99f199ca489a072d07e2449a2765e
maximterentevasu-byte <maximterentevasu@gmail.com>
2026-03-26 21:36:39 +0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,11 +1,43 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Привет 👋'));
+// Главное меню
+function mainMenu(ctx) {
+    ctx.reply(
+        'Привет! Я бот бонусной программы Pick me. Выбери интересующий тебя пункт:',
+        Markup.keyboard([
+            ['Подключиться к бонусной программе'],
+            ['Скачать бонусную карту на телефон']
+        ])
+        .resize()
+    );
+}
 
-bot.on('text', (ctx) => {
-    ctx.reply('Ты написал: ' + ctx.message.text);
+// Команда /start
+bot.start((ctx) => {
+    mainMenu(ctx);
+});
+
+// Кнопка 1
+bot.hears('Подключиться к бонусной программе', (ctx) => {
+    ctx.reply(
+        'Для регистрации в бонусной программе заполни анкету:\nhttps://card.evobonus.ru/form/74e48448-1975-4bca-a455-92ec9a4bbf76',
+        Markup.keyboard([
+            ['На главный экран']
+        ])
+        .resize()
+    );
+});
+
+// Кнопка назад
+bot.hears('На главный экран', (ctx) => {
+    mainMenu(ctx);
+});
+
+// Кнопка 2 (пока заглушка)
+bot.hears('Скачать бонусную карту на телефон', (ctx) => {
+    ctx.reply('Ссылка для скачивания карты скоро появится');
 });
 
 bot.launch();
